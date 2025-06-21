b4407bf501e20f80260f7ea45eaefee614fc978c
S-Marin0 <smarinp@udistrital.edu.co>
2025-06-21 15:03:02 -0500

    Update SistemaFinancieroFacade.java

--- SistemaFinancieroFacade.java
+++ SistemaFinancieroFacade.java
@@ -1,7 +1,9 @@
 package fachada;
 
 import Interfaz.InterfazFinanciera;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import modelos.MetaFinanciera;
@@ -25,15 +27,16 @@ public class SistemaFinancieroFacade {
     }
     
     // Métodos para Transacciones
-    public void registrarIngreso(String descripcion, double monto, String categoria) {
+    public boolean registrarIngreso(String descripcion, double monto, String categoria) {
         Transaccion ingreso = new Transaccion(descripcion, monto, categoria, Transaccion.TipoTransaccion.INGRESO);
         gestorTransacciones.agregarTransaccion(ingreso);
+        return true;
     }
     
-    public void registrarGasto(String descripcion, double monto, String categoria) {
+    public boolean registrarGasto(String descripcion, double monto, String categoria) {
         Transaccion gasto = new Transaccion(descripcion, monto, categoria, Transaccion.TipoTransaccion.GASTO);
         gestorTransacciones.agregarTransaccion(gasto);
-        gestorPresupuestos.registrarGastoEnPresupuesto(categoria, monto);
+        return true;
     }
     
     public List<Transaccion> obtenerTransacciones() {
@@ -55,15 +58,17 @@ public class SistemaFinancieroFacade {
     public Map<String, Double> obtenerGastosPorCategoria() {
         return gestorTransacciones.obtenerGastosPorCategoria();
     }
-    
+     
     // Métodos para Metas
-    public void crearMeta(String nombre, double montoObjetivo, LocalDate fechaLimite, String descripcion) {
+    public boolean crearMeta(String nombre, double montoObjetivo, LocalDate fechaLimite, String descripcion) {
         MetaFinanciera meta = new MetaFinanciera(nombre, montoObjetivo, fechaLimite, descripcion);
         gestorMetas.agregarMeta(meta);
+        return true;
     }
     
-    public void contribuirAMeta(String nombreMeta, double monto) {
+    public boolean contribuirAMeta(String nombreMeta, double monto) {
         gestorMetas.contribuirAMeta(nombreMeta, monto);
+        return true;
     }
     
     public List<MetaFinanciera> obtenerMetas() {
@@ -74,19 +79,17 @@ public class SistemaFinancieroFacade {
         return gestorMetas.obtenerMetasEnProgreso();
     }
     
+    public List<MetaFinanciera> obtenerMetasCompletadas() {
+        return gestorMetas.obtenerMetasCompletadas();
+    }
+    
     // Métodos para Presupuestos
-    public void crearPresupuesto(String nombre, int mes, int año) {
+    public boolean crearPresupuesto(String nombre, int mes, int año) {
         Presupuesto presupuesto = new Presupuesto(nombre, mes, año);
         gestorPresupuestos.agregarPresupuesto(presupuesto);
+        return true;
     }
-    
-    public void agregarCategoriaAPresupuesto(String nombrePresupuesto, String categoria, double limite) {
-        gestorPresupuestos.obtenerPresupuestos().stream()
-                .filter(p -> p.getNombre().equals(nombrePresupuesto))
-                .findFirst()
-                .ifPresent(p -> p.agregarLimiteCategoria(categoria, limite));
-    }
-    
+
     public List<Presupuesto> obtenerPresupuestos() {
         return gestorPresupuestos.obtenerPresupuestos();
     }
@@ -95,8 +98,19 @@ public class SistemaFinancieroFacade {
         return gestorPresupuestos.obtenerPresupuestoActual();
     }
     
+    public boolean registrarGastoEnPresupuesto(String categoria, double monto) {
+        gestorPresupuestos.registrarGastoEnPresupuesto(categoria, monto);
+        return true;
+    }
+    
     // Método para mostrar la interfaz
     public void mostrarInterfaz() {
         interfaz.setVisible(true);
+    }
+    
+    // Método para obtener el último error (opcional)
+    public String obtenerUltimoError() {
+        // Podrías implementar un sistema de manejo de errores más sofisticado
+        return "Consulta los logs de la consola para más detalles";
     }
 }
