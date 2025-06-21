6ce8a724eaac106b6520c6ab34fc7ba1fa210e62
S-Marin0 <smarinp@udistrital.edu.co>
2025-06-21 14:40:24 -0500

    Update GestorMetas.java

--- GestorMetas.java
+++ GestorMetas.java
@@ -1,40 +1,77 @@
 package servicios;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.sql.SQLException;
+import java.util.List;
+import database.dao.MetaDAO;
 import modelos.MetaFinanciera;
 
 public class GestorMetas {
-    private List<MetaFinanciera> metas;
+    private MetaDAO metaDAO;
     
     public GestorMetas() {
-        this.metas = new ArrayList<>();
+        this.metaDAO = new MetaDAO();
     }
     
-    public void agregarMeta(MetaFinanciera meta) {
-        metas.add(meta);
+    /**
+     * Agrega una nueva meta financiera a la base de datos
+     */
+    public void agregarMeta(MetaFinanciera meta) throws SQLException {
+        metaDAO.insertarMeta(meta);
     }
     
-    public List<MetaFinanciera> obtenerMetas() {
-        return new ArrayList<>(metas);
+    /**
+     * Obtiene todas las metas financieras de la base de datos
+     */
+    public List<MetaFinanciera> obtenerMetas() throws SQLException {
+        return metaDAO.obtenerTodasMetas();
     }
     
-    public List<MetaFinanciera> obtenerMetasCompletadas() {
-        return metas.stream()
-                .filter(MetaFinanciera::estaCompletada)
-                .collect(Collectors.toList());
+    /**
+     * Obtiene solo las metas completadas
+     */
+    public List<MetaFinanciera> obtenerMetasCompletadas() throws SQLException {
+        return metaDAO.obtenerMetasCompletadas();
     }
     
-    public List<MetaFinanciera> obtenerMetasEnProgreso() {
-        return metas.stream()
-                .filter(m -> !m.estaCompletada())
-                .collect(Collectors.toList());
+    /**
+     * Obtiene solo las metas en progreso (no completadas)
+     */
+    public List<MetaFinanciera> obtenerMetasEnProgreso() throws SQLException {
+        return metaDAO.obtenerMetasEnProgreso();
     }
     
-    public void contribuirAMeta(String nombreMeta, double monto) {
-        metas.stream()
-                .filter(m -> m.getNombre().equals(nombreMeta))
-                .findFirst()
-                .ifPresent(m -> m.agregarMonto(monto));
+    /**
+     * Contribuye un monto a una meta específica
+     */
+    public void contribuirAMeta(String nombreMeta, double monto) throws SQLException {
+        metaDAO.contribuirAMeta(nombreMeta, monto);
+    }
+    
+    /**
+     * Actualiza el monto actual de una meta (reemplaza el valor existente)
+     */
+    public void actualizarMontoMeta(String nombreMeta, double nuevoMonto) throws SQLException {
+        metaDAO.actualizarMontoMeta(nombreMeta, nuevoMonto);
+    }
+    
+    /**
+     * Elimina una meta de la base de datos
+     */
+    public void eliminarMeta(String nombreMeta) throws SQLException {
+        metaDAO.eliminarMeta(nombreMeta);
+    }
+    
+    /**
+     * Obtiene una meta específica por su nombre
+     */
+    public MetaFinanciera obtenerMetaPorNombre(String nombre) throws SQLException {
+        return metaDAO.obtenerMetaPorNombre(nombre);
+    }
+    
+    /**
+     * Verifica si una meta existe en la base de datos
+     */
+    public boolean existeMeta(String nombreMeta) throws SQLException {
+        return metaDAO.obtenerMetaPorNombre(nombreMeta) != null;
     }
 }
